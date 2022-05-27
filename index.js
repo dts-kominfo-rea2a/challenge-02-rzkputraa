08f9a5a9ddd621c1af8280915298b2ebc5def17a
Rizki Putra W <rizkiputra1100@gmail.com>
2022-05-27 22:57:12 +0700

    mengerjakan tugas alur logika

--- index.js
+++ index.js
@@ -25,17 +25,54 @@ function lakukanLooping(arrPegawai) {
   */
   let hasilLooping = null;
 
+  hasilLooping = [];
+
+  for (
+    let iteration = 0;
+    iteration < dataYangAkanDilooping.length;
+    iteration++
+  ) {
+    hasilLooping.push(
+      `${dataYangAkanDilooping[iteration].namaDepan} ${dataYangAkanDilooping[iteration].namaBelakang}`
+    );
+  }
+
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
 
+  jumlahPria = 0;
+
+  for (
+    let iteration = 0;
+    iteration < dataYangAkanDilooping.length;
+    iteration++
+  ) {
+    if (dataYangAkanDilooping[iteration].jenisKelamin === "M") {
+      jumlahPria++;
+    }
+  }
+
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
+
   let jumlahWanita = null;
+
+  jumlahWanita = 0;
+
+  for (
+    let iteration = 0;
+    iteration < dataYangAkanDilooping.length;
+    iteration++
+  ) {
+    if (dataYangAkanDilooping[iteration].jenisKelamin === "F") {
+      jumlahWanita++;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +86,16 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+
+  komentar = "";
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
